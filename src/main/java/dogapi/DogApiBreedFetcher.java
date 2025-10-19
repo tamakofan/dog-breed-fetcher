7c0119dc858713b4e3739da561cde9db9b9c8ead
tamakofan <tamako.fan@mail.utoronto.ca>
2025-10-18 22:59:29 -0400

    dogapibreedfetcher

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -25,7 +25,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+    public List<String> getSubBreeds(String breed) throws RuntimeException {
         HttpUrl url = new HttpUrl.Builder()
                 .scheme("https")
                 .host("dog.ceo")
@@ -39,20 +39,24 @@ public class DogApiBreedFetcher implements BreedFetcher {
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            String data = response.body().string(); // I need to find a way to take out just the list
-            List<String> subbreeds = Arrays.asList(data.substring(11, -20)); // can't have a negative apparently
-            // ArrayList rtnlst = new ArrayList<String>();
-            // for (int j = 0; j < lst.size(); j++) {
-            //    rtnlst.add(lst.get(j));
-            return Collections.singletonList(data);
-            } catch (IOException e) {
+            assert response.body() != null;
+            String data = response.body().string();
+
+            JSONObject jsonObject = new JSONObject(data);
+            String status = jsonObject.getString("status");
+
+            if (status.equals("error")) {
+                throw new BreedNotFoundException(breed);
+            }
+            else {
+                JSONArray messageArray = jsonObject.getJSONArray("message");
+                List<String> subbreeds = new ArrayList<>();
+                for (int j = 0; j < messageArray.length(); j++) {
+                subbreeds.add(messageArray.getString(j));}
+                return subbreeds;
+            }
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
     }
 }
